5dc83cd29aaed7339369a1e716df26c2f6b5cecc
luqmanfifi12 <92702782+luqmanfifi12@users.noreply.github.com>
2021-10-29 16:43:00 +0800

    Initial commit

---
+++ Test Suites/TestSuites1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuites1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0891ab6e-bb13-42f4-acf3-3486672bbdf2</testSuiteGuid>
+   <testCaseLink>
+      <guid>4bd27f0a-82f5-4e40-8041-ad9d0d2d1b02</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RunFeatureFile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
